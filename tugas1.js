c1e3edeabb7a30964c5aa5409792443d734f4c53
Zakaria Ahmada <127176536+zakaria1909@users.noreply.github.com>
2023-05-09 06:14:29 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,6 @@
+const firstname = "Zakaria Ahmada",
+        lastname = "Zaka";
+        let age = "19";
+        const ismarried = "BELUM MENIKAH";
+        console.log(`Hallo, Nama Saya ${firstname}. Biasa dipanggil ${lastname}. Saya berumur ${age} Dan status saya masih ${ismarried}`);
+        
